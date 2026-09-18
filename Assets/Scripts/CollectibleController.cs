e8a0cd0bef02d3cc2823e227cebfe0d53a17e9e2
PaTrIKWhItNeY <pwhitney30@sandomenico.org>
2026-09-18 09:30:39 -0700

    Made A Collectible

---
+++ Assets/Scripts/CollectibleController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class CollectibleController : MonoBehaviour
+{
+    [SerializeField] private AudioClip collectSound;
+    [SerializeField] private GameObject collectParticlePrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+      // Call with this object collides with a trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        // Only executes if the collision was with the Player
+        if (other.CompareTag("Player"))
+        {
+            // Spawn audio at the collectible's position (auto-destroys)
+            AudioSource.PlayClipAtPoint(collectSound, transform.position);
+
+            // Spawn particles (auto-destroys if Stop Action is set to Destroy)
+            Instantiate(collectParticlePrefab, transform.position, Quaternion.identity);
+
+            // Safely destroy the collectible immediately
+            Destroy(gameObject);
+        }
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,7 +21,7 @@ public class PlayerController : MonoBehaviour
   // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       force = 5.25f;
+       force = 10.25f;
        jumpForce = 150.8f;
     }
 
